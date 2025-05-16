abd37dfb749661b83288cec472529e74603162c3
Adam Svoboda <svobodaad.06@spst.eu>
2025-05-16 11:32:37 +0200

    Add files via upload

---
+++ Test_txt/Test_txt/Test_txt.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Test_txt
+{
+    partial class Test_txt
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "projít txt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(353, 303);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "výpis celého souboru";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(392, 108);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(396, 303);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(523, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "lide s menší než minimální mzdou";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Test_txt/Test_txt/Test_txt.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Test_txt
+{
+    public partial class Test_txt : Form
+    {
+        public Test_txt()
+        {
+            this.Text = "Test txt";
+            this.ShowIcon = false;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (StreamReader ctenar = new StreamReader("soubor.txt"))
+            {
+                while (!ctenar.EndOfStream)
+                {
+                    String line = ctenar.ReadLine();
+                    listBox1.Items.Add(line + "\r\n");
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pocet_zen = 0;
+            int vek_soucet = 0;
+            int vek_pocet = 100;
+            int mzda_max = int.MinValue;
+            if (File.Exists("soubor.txt"))
+            {
+                using (StreamReader ctenar = new StreamReader("soubor.txt"))
+                {
+                    while (!ctenar.EndOfStream)
+                    {
+                        string gender = string.Empty;
+                        string vek = string.Empty;
+                        string mzda = string.Empty;
+                        string line = ctenar.ReadLine();
+                        int pocet = 0;
+
+                        foreach (char znaky in line)
+                        {
+                            if (znaky == '"')
+                            {
+                                pocet++;
+                            }
+                            if (pocet == 7)
+                            {
+                                while (znaky != '"')
+                                {
+                                    gender += znaky;
+                                }
+                                if (gender == "Female")
+                                {
+                                    pocet_zen++;
+                                    gender = string.Empty;
+                                }
+                                else
+                                {
+                                    gender = string.Empty;
+                                }
+
+                            }
+
+                            else if (pocet == 11)
+                            {
+                                while (znaky != '"')
+                                {
+                                    vek += znaky;
+                                }
+                            }
+
+                            else if (pocet == 15)
+                            {
+                                while (znaky != '"')
+                                {
+                                    mzda += znaky;
+                                }
+                                if (Convert.ToInt32(mzda) < 15200)
+                                {
+                                    listBox2.Items.Add(line);
+                                }
+                                else if (Convert.ToInt32(mzda) > mzda_max)
+                                {
+                                    mzda_max = Convert.ToInt32(mzda);
+                                }
+                            }
+                        }
+                    }
+                }
+           
+                MessageBox.Show("Počet vygenerovaných žen: " + pocet_zen);
+                double prumer = vek_soucet / vek_pocet;
+
+                using (StreamWriter zapisnik = new StreamWriter("best.txt"))
+                {
+                    zapisnik.Write("Průměrný věk: " + prumer + "\r\n");
+                    zapisnik.Write("Největší výplata: " + mzda_max + "Kč \r\n");
+                }
+            }
+                    else
+                    {
+                        MessageBox.Show("Soubor nebyl nalezen");
+                    }
+        }
+    } 
+}
+
+
+
+  
+       
+      
+        
+
+       
+
+
+
